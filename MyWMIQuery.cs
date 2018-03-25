fc7146d43f1c6333724c6cfebbcdb7b3717fdb61
pekto12 <34380232+pekto12@users.noreply.github.com>
2018-03-25 13:25:44 +0200

    Add files via upload
    Odczytuje temperaturę i wyświetla

---
+++ MyWMIQuery.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Management;
+using System.Windows.Forms;
+
+namespace WMISample
+{
+    public class MyWMIQuery
+    {
+        public static void Main()
+        {
+            try
+            {
+                ManagementObjectSearcher searcher = 
+                    new ManagementObjectSearcher("root\\WMI", 
+                    "SELECT * FROM MSAcpi_ThermalZoneTemperature"); 
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    Console.WriteLine("-----------------------------------");
+                    Console.WriteLine("MSAcpi_ThermalZoneTemperature instance");
+                    Console.WriteLine("-----------------------------------");
+                    Console.WriteLine("CurrentTemperature: {0}", queryObj["CurrentTemperature"]);
+                }
+            }
+            catch (ManagementException e)
+            {
+                MessageBox.Show("An error occurred while querying for WMI data: " + e.Message);
+            }
+        }
+    }
+}
